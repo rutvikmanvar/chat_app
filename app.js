72e04f762b0cdc0dead166e9b1ec1894f1e64edf
Rutvik <rutvikmanvar99@gmail.com>
2025-09-02 22:13:28 +0530

    socket changes

--- app.js
+++ app.js
@@ -24,51 +24,109 @@ app.use('/',userRoute)
 
 const io = require('socket.io')(http);
 
-const userNameSpace = io.of('/user-namespace')
+// const userNameSpace = io.of('/user-namespace')
 
-userNameSpace.on('connection',async function(socket){
-    console.log('User connected');
-    console.log(`user id = ${socket.handshake.query.token}`)
+// userNameSpace.on('connection',async function(socket){
+//     console.log('User connected');
+//     console.log(`user id = ${socket.handshake.query.token}`)
+
+//     var userId = socket.handshake.query.token;
+
+//     await User.findByIdAndUpdate({_id:userId},{$set:{is_online:'1'}})
+
+//     //user broadcast online status
+//     socket.broadcast.emit('getOnlineUser',{user_id:userId})
+
+//     socket.on('disconnect',async function(){
+//         console.log('User disconnected');
+//         await User.findByIdAndUpdate({_id:userId},{$set:{is_online:'0'}})
+
+//         //user broadcast offline status
+//         socket.broadcast.emit('getOfflineUser',{user_id:userId})
+//     });
+
+//     //chating implementation
+
+//     socket.on('newChat',function(data){
+//         socket.broadcast.emit('loadNewChat',data);
+//     })
+
+//     //load old chat
+//     socket.on('existsChat',async function(data){
+//         var chats = await Chat.find({
+//             $or:[
+//                 {sender_id:data.sender_id,receiver_id:data.receiver_id},
+//                 {sender_id:data.receiver_id,receiver_id:data.sender_id},
+//             ]
+//         });
+
+//         socket.emit('loadChats',{chats:chats});
+//     });
+
+//     //delete chat
+//     socket.on('chatDeleted',function(id){
+//         socket.broadcast.emit('chatMessageDeleted',id);
+//     });
+
+// });
+
+const userNameSpace = io.of('/user-namespace')
+userNameSpace.on('connection', async function (socket) {
+    console.log('User connected:', socket.id);
 
     var userId = socket.handshake.query.token;
+    await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: '1' } });
 
-    await User.findByIdAndUpdate({_id:userId},{$set:{is_online:'1'}})
+    // Join user-specific room
+    socket.join(userId);
+    console.log(`User ${userId} joined room`);
 
-    //user broadcast online status
-    socket.broadcast.emit('getOnlineUser',{user_id:userId})
+    // Broadcast online status
+    socket.broadcast.emit('getOnlineUser', { user_id: userId });
 
-    socket.on('disconnect',async function(){
-        console.log('User disconnected');
-        await User.findByIdAndUpdate({_id:userId},{$set:{is_online:'0'}})
+    // Disconnect
+    socket.on('disconnect', async function () {
+        console.log('User disconnected:', socket.id);
+        await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: '0' } });
 
-        //user broadcast offline status
-        socket.broadcast.emit('getOfflineUser',{user_id:userId})
+        socket.broadcast.emit('getOfflineUser', { user_id: userId });
     });
 
-    //chating implementation
+    // Chatting
+    socket.on('newChat', async function (data) {
+        // 1. Save message
+        const chat = new Chat({
+            sender_id: data.sender_id,
+            receiver_id: data.receiver_id,
+            message: data.message,
+        });
+        await chat.save();
 
-    socket.on('newChat',function(data){
-        socket.broadcast.emit('loadNewChat',data);
-    })
+        // 2. Emit to receiver's room
+        userNameSpace.to(data.receiver_id).emit('loadNewChat', data);
 
-    //load old chat
-    socket.on('existsChat',async function(data){
+        // 3. Emit back to sender (ack)
+        socket.emit('loadNewChat', data);
+    });
+
+    // Load old chat
+    socket.on('existsChat', async function (data) {
         var chats = await Chat.find({
-            $or:[
-                {sender_id:data.sender_id,receiver_id:data.receiver_id},
-                {sender_id:data.receiver_id,receiver_id:data.sender_id},
-            ]
+            $or: [
+                { sender_id: data.sender_id, receiver_id: data.receiver_id },
+                { sender_id: data.receiver_id, receiver_id: data.sender_id },
+            ],
         });
 
-        socket.emit('loadChats',{chats:chats});
+        socket.emit('loadChats', { chats: chats });
     });
 
-    //delete chat
-    socket.on('chatDeleted',function(id){
-        socket.broadcast.emit('chatMessageDeleted',id);
+    // Delete chat
+    socket.on('chatDeleted', function (id) {
+        socket.broadcast.emit('chatMessageDeleted', id);
     });
-
 });
+
 
 http.listen(PORT,()=>{
     console.log(`Server running on PORT ${PORT}`);
